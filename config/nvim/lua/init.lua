cb192c9de67734e0843e26411db08efff93f3374
David Susskind <david.susskind@walkme.com>
2022-12-14 15:40:24 +0200

    make syntax highlight work on vim enter ( need to defer treesitter setup... WTF!!!)

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -17,34 +17,37 @@ augroup END
 require('spaceline')
 require('lsp_configuration')
 if isModuleAvailable('nvim-treesitter.configs') then
-  require'nvim-treesitter.configs'.setup {
-    ensure_installed = { "typescript", "lua", "vim", "python" },
-    highlight = { 
-      enable = true,
-      disable = {'json', 'jsonc'}
-    }, 
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "gnn",
-        node_incremental = "+",
-        scope_incremental = "-",
-        node_decremental = "_",
+  -- annoying: need to defer setting config of treesitter otherwise syntax highlight fails for first buffer
+  vim.defer_fn(function()
+    require'nvim-treesitter.configs'.setup {
+      ensure_installed = { "javascript", "typescript", "lua", "vim", "python" },
+      highlight = { 
+        enable = true,
+        disable = {'json', 'jsonc'}
+      }, 
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "gnn",
+          node_incremental = "+",
+          scope_incremental = "-",
+          node_decremental = "_",
+        },
       },
-    },
-  }
-  local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-  parser_config.dockerfile = {
-    install_info = {
-      url = "https://github.com/camdencheek/tree-sitter-dockerfile", -- local path or git repo
-      files = {"src/parser.c"},
-      -- optional entries:
-      branch = "main", -- default branch in case of git repo if different from master
-      generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-      requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
-    },
-    filetype = "dockerfile", -- if filetype does not match the parser name
-  }
+    }
+    local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+    parser_config.dockerfile = {
+      install_info = {
+        url = "https://github.com/camdencheek/tree-sitter-dockerfile", -- local path or git repo
+        files = {"src/parser.c"},
+        -- optional entries:
+        branch = "main", -- default branch in case of git repo if different from master
+        generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+        requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
+      },
+      filetype = "dockerfile", -- if filetype does not match the parser name
+    }
+  end, 10)
 end
 -- vim.opt.foldmethod = 'expr'
 -- vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
