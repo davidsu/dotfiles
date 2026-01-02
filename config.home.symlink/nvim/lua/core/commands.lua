ec142c8794ae2fbde3daf0acbfc1e329a0d88b3a
David Susskind <susskinddavid@gmail.com>
2026-01-02 02:12:11 +0200

    refactor(nvim): remove obsolete FoldCommentsToggle command
    Remove FoldCommentsToggle user command since we switched to LSP-based folding.

--- config.home.symlink/nvim/lua/core/commands.lua
+++ config.home.symlink/nvim/lua/core/commands.lua
@@ -1,18 +1,6 @@
 -- Core User Commands
 -- User-defined commands (not keymaps)
 
-local comment_folds_visible = true
-vim.api.nvim_create_user_command('FoldCommentsToggle', function()
-  local folding = require('utils.folding')
-  if comment_folds_visible then
-    folding.fold_comments_only()
-    comment_folds_visible = false
-  else
-    folding.unfold_comments_only()
-    comment_folds_visible = true
-  end
-end, { desc = 'Toggle fold state of comment blocks' })
-
 vim.api.nvim_create_user_command('Diagnostics', function()
   vim.diagnostic.setqflist()
 end, { desc = 'Show all diagnostics in quickfix list' })
