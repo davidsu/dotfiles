9ba2550d811d69d70538c2c0315a1c756c45dfe9
David Susskind <susskinddavid@gmail.com>
2026-01-03 03:04:04 +0200

    Refactor nvim-tree config to reduce nesting

--- config.home.symlink/nvim/lua/plugins/tree.lua
+++ config.home.symlink/nvim/lua/plugins/tree.lua
@@ -7,50 +7,36 @@ if env.is_vscode then
   return {}
 end
 
+local function find_file_in_tree()
+  require('utils.git').cd_to_buffer_root()
+  vim.cmd('NvimTreeFindFile')
+end
+
+local keys = {
+  { '1n', '<cmd>NvimTreeToggle<cr>', desc = 'Toggle file tree' },
+  { '<space>nf', find_file_in_tree, desc = 'Find file in tree' },
+}
+
+local opts = {
+  disable_netrw = true,
+  hijack_netrw = true,
+  view = { width = 40, side = 'left' },
+  renderer = {
+    group_empty = true,
+    highlight_git = true,
+    icons = { show = { git = true, folder = true, file = true, folder_arrow = true } },
+  },
+  filters = { dotfiles = false, custom = {} },
+  git = { enable = true, ignore = false },
+  actions = { open_file = { quit_on_open = false, resize_window = true } },
+}
+
 return {
   {
     'nvim-tree/nvim-tree.lua',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    keys = {
-      { '1n', '<cmd>NvimTreeToggle<cr>', desc = 'Toggle file tree' },
-      { '<space>nf', '<cmd>NvimTreeFindFile<cr>', desc = 'Find file in tree' },
-    },
-    config = function()
-      require('nvim-tree').setup({
-        disable_netrw = true,
-        hijack_netrw = true,
-        view = {
-          width = 40,
-          side = 'left',
-        },
-        renderer = {
-          group_empty = true,
-          highlight_git = true,
-          icons = {
-            show = {
-              git = true,
-              folder = true,
-              file = true,
-              folder_arrow = true,
-            },
-          },
-        },
-        filters = {
-          dotfiles = false,
-          custom = {},  -- Show ALL files, no filtering
-        },
-        git = {
-          enable = true,
-          ignore = false,  -- Don't hide gitignored files
-        },
-        actions = {
-          open_file = {
-            quit_on_open = false,
-            resize_window = true,
-          },
-        },
-      })
-    end,
+    keys = keys,
+    opts = opts,
   },
 }
 
