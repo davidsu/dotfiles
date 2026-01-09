47ad2c113214234b3d6f1814fd12d45c2ba5beb6
David Susskind <susskinddavid@gmail.com>
2026-01-09 17:32:23 +0200

    refactor: folding is a big enough concern, give it its own file

---
+++ config.home.symlink/nvim/lua/plugins/folding.lua
@@ -0,0 +1,40 @@
+-- Folding with nvim-ufo
+
+local env = require('core.env')
+
+if env.is_vscode then
+  return {}
+end
+
+local function config()
+  vim.o.foldcolumn = '0'
+  vim.o.foldlevel = 99
+  vim.o.foldlevelstart = 99
+  vim.o.foldenable = true
+
+  require('ufo').setup({
+    open_fold_hl_timeout = 0, -- disable highlight flash when opening folds
+    provider_selector = function(bufnr, filetype, buftype)
+      if filetype == 'markdown' then
+        return { 'treesitter', 'indent' }
+      end
+      return { 'lsp', 'indent' }
+    end,
+  })
+
+  -- Remap zM and zR to use ufo's functions instead of native vim commands
+  -- This keeps foldlevel high, preventing auto-closing folds when buffer is modified
+  vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = 'Open all folds' })
+  vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = 'Close all folds' })
+  vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds, { desc = 'Open folds incrementally' })
+  vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith, { desc = 'Close folds incrementally' })
+end
+
+return {
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = { 'kevinhwang91/promise-async' },
+    event = 'VeryLazy',
+    config = config,
+  },
+}
--- config.home.symlink/nvim/lua/plugins/ui.lua
+++ config.home.symlink/nvim/lua/plugins/ui.lua
@@ -7,27 +7,6 @@ if env.is_vscode then
   return {}
 end
 
-local function ufo_config()
-  vim.o.foldcolumn = '0'
-  vim.o.foldlevel = 99
-  vim.o.foldlevelstart = 99
-  vim.o.foldenable = true
-
-  require('ufo').setup({
-    open_fold_hl_timeout = 0, -- disable highlight flash when opening folds
-    provider_selector = function(bufnr, filetype, buftype)
-      return { 'lsp', 'indent' }
-    end,
-  })
-
-  -- Remap zM and zR to use ufo's functions instead of native vim commands
-  -- This keeps foldlevel high, preventing auto-closing folds when buffer is modified
-  vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = 'Open all folds' })
-  vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = 'Close all folds' })
-  vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds, { desc = 'Open folds incrementally' })
-  vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith, { desc = 'Close folds incrementally' })
-end
-
 return {
   -- Smooth scrolling
   {
@@ -42,13 +21,5 @@ return {
     config = function()
       vim.g.diminactive_enable_focus = 1
     end,
-  },
-
-  -- Better folding with VSCode-like fold UI
-  {
-    'kevinhwang91/nvim-ufo',
-    dependencies = { 'kevinhwang91/promise-async' },
-    event = 'VeryLazy',
-    config = ufo_config,
   },
 }
