e9e6fd858432142fe9037c53522c60d08a0737f7
David Susskind <dev@example.com>
2026-01-23 16:13:53 +0200

    nuke dock for new mac

--- installation/macos-defaults.sh
+++ installation/macos-defaults.sh
@@ -16,9 +16,13 @@ defaults write com.apple.finder DisableAllAnimations -bool true
 # Fast keyboard repeat
 defaults write NSGlobalDomain KeyRepeat -int 1
 
-# Dock: scale effect, fast autohide
+# Dock: scale effect, fast autohide, and clear default apps
 defaults write com.apple.dock mineffect -string "scale"
 defaults write com.apple.dock autohide-time-modifier -float 0
+
+# Clear all pinned apps and hide "recent" apps section
+defaults write com.apple.dock persistent-apps -array
+defaults write com.apple.dock show-recents -bool false
 
 # Restart affected apps
 killall Dock Finder 2>/dev/null
